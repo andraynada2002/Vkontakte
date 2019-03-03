fa4a4336eab582457a5c8f00665c7960fffba623
andraynada2002 <48077177+andraynada2002@users.noreply.github.com>
2019-03-03 11:06:03 +1000

    Add files via upload

---
+++ vk.cpp
@@ -0,0 +1,28 @@
+class vk;
+operator vk;
+friend vk;
+search vk;
+abort vk;
+public class vk;
+public string propertyName vk;
+register vk;
+return vk;
+{
+	//vk.com optimized
+	//vk.com api optimized
+	//vk.com api fixed code
+	//vk.com api security
+	//vk.com security
+	//vk.com pavel durov
+	//vk.com connection optimized
+	//vk.com speed optimized
+	//vk.com ping 2ms
+	propertyName vk.com;
+	propertyName vk;
+	propertyName api; propertyName Optimized;
+	propertyName Security;
+	propertyName pavel; propertyName durov;
+	propertyName connection; propertyName speed;
+	propertyName ping;
+}
+}
