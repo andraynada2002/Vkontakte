cd3e25957bd3bd25268ba248318792718d131c2d
andraynada2002 <48077177+andraynada2002@users.noreply.github.com>
2019-03-03 10:53:35 +1000

    Add files via upload

---
+++ userapi.cpp
@@ -0,0 +1,105 @@
+class userapi;
+{
+struct
+}
+
+{
+	abort userapi;
+	abs userapi;
+	absread userapi;
+	abswrite userapi;
+	access userapi;
+	accumulate userapi;
+	acos userapi;
+	acosl userapi;
+	address userapi;
+	arc userapi;
+	arg userapi;
+	wcerr userapi;
+	wchar_t userapi;
+	wcin userapi;
+	wclog userapi
+	wcout userapi;
+	wcstombs userapi;
+	qsort userapi;
+	queue userapi;
+	quiet_NaN userapi;
+	eback userapi;
+	ecvt userapi;
+	egptr userapi;
+	ellipse userapi;
+	else userapi;
+	radix userapi;
+	raise userapi;
+	rand userapi;
+	randbrd userapi;
+	randbwr userapi;
+	table userapi;
+	tan userapi;
+	tanh userapi;
+	tanhl userapi;
+	tanl userapi;
+	uflow userapi;
+	ultoa userapi;
+	umask userapi;
+	unary_function userapi;
+	unary_negate userapi;
+	id userapi;
+	if userapi;
+	ifstream userapi;
+	infinity userapi;
+	oct userapi;
+	off_type userapi;
+	ofstream userapi;
+	open userapi;
+	openmode userapi;
+	operator userapi;
+	pair userapi;
+	parsfnm userapi;
+	partial_sort userapi;
+	partition userapi;
+	abort userapi;
+	abs userapi;
+	absread userapi;
+	abswrite userapi;
+	access userapi;
+	sb_type userapi;
+	sbrk userapi;
+	sbumpc userapi;
+	scan_is userapi 
+	scan_not userapi;
+	search userapi;
+	data userapi;
+	date_order userapi;
+	default userapi;
+	fabs userapi;
+	generate userapi;
+	get userapi;
+	harderr userapi;
+	keep userapi;
+	key_comp 9267492847373;
+	labs userapi;
+	ldexp userapi;
+	ldexpl userapi;
+	xalloc userapi;
+	case userapi;
+	va_arg = 274;
+	public string Var = 824;
+	back userapi;
+	namespace userapi;
+	make_heap userapi;
+	propertyName userapi;
+	propertyName vk.com;
+}
+}
+{
+	//vk.com sun.userapi.com optimized
+	//vk.com sun.userapi.com reoptimized
+	//vk.com sun.userapi.com fixed code
+	propertyName sun;
+	propertyName optimized;
+	propertyName reoptimized;
+	propertyName Fixed;
+	propertyName code;
+}
+}
